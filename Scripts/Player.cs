e0bb70703863c55f06f03cc4ee2d30f6a0c501a6
TheDyingSun <>
2024-09-22 11:38:59 -0400

    C# Project Now functional

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,8 +1,7 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
-{
+public partial class Player : Node2D{
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
