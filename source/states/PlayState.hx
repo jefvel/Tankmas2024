4c9931bf4a0de433ffc998e3959d22e7986a15a4
chosencharacters <chosencharacter@gmail.com>
2024-11-05 22:07:14 -0500

    basic bg

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -9,6 +9,8 @@ class PlayState extends BaseState
 {
 	public static var self:PlayState;
 
+	var bg:FlxSpriteExt;
+
 	public var players:FlxTypedGroup<Player> = new FlxTypedGroup<Player>();
 	public var presents:FlxTypedGroup<Present> = new FlxTypedGroup<Present>();
 	public var player_shadows:FlxTypedGroup<FlxSpriteExt> = new FlxTypedGroup<FlxSpriteExt>();
@@ -20,12 +22,19 @@ class PlayState extends BaseState
 		
 		bgColor = FlxColor.GRAY;
 
+		add(bg = new FlxSpriteExt(Paths.get("bg-outside-hotel.png")));
+
 		add(player_shadows);
 		add(presents);
 		add(players);
 
 		new Player();
 		new Present();
+		players.getFirstAlive().center_on(bg);
+
+		FlxG.camera.target = players.getFirstAlive();
+
+		FlxG.camera.setScrollBounds(bg.x, bg.width, bg.y, bg.height);
 	}
 
 	override public function update(elapsed:Float)
